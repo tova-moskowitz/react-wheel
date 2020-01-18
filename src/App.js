9053c91185b7185c9a3b2b786e02b7b434fe9fb3
= <=>
2020-01-18 14:17:50 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './components/PuzzleBoard/PuzzleBoard.css';
+import phrases from './puzzleBank/phrases.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component  {
+  constructor(props) {
+  super(props);
+  // this.state = {currentPuzzle: }
+  }
+
+  preparePuzzle() {
+      let puzzle = phrases[Math.floor(Math.random() * phrases.length)];
+      puzzle = puzzle.split('');
+      return puzzle.map(function(letter, index){
+        if(letter !== ' '){
+          return (
+            <div key={index} className="puzzleWrapper">
+              <div className="puzzleLetter">{letter.toUpperCase()}</div>
+            </div>
+          )
+        }
+        else {
+          return(
+            <div key={index}>
+              &nbsp;
+            </div>
+          )
+        }
+      });
+    }
+
+
+  render() {
+      return (
+      <div className="PuzzleBoard">
+        <div className="puzzleBoard">
+            {this.preparePuzzle() }
+            </div>
+            <div>
+              <button>Get New Puzzle</button>
+            </div>
+      </div>
+    );
+  }
 }
-
 export default App;
