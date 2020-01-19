31e52fb51c2b4671a4c8a68a65040e06697d7477
= <=>
2020-01-18 21:37:15 -0500

    move everything into App.js

---
+++ src/components/RefreshPuzzleButton/index.js
@@ -0,0 +1,14 @@
+// import React, {Component} from 'react';
+// import phrases from '../../puzzleBank/phrases.js';
+//
+// class RefreshPuzzleButton extends Component {
+//     render() {
+//       return(
+//         <div>
+//           <button>Get New Puzzle</button>
+//         </div>
+//       )
+//     }
+// }
+//
+// export default RefreshPuzzleButton;
