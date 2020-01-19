dc6731f3a8c62838e5f5bb1094a48d729b39cbc4
= <=>
2020-01-18 21:32:32 -0500

    reformatted and put everything into App.js

--- src/App.js
+++ src/App.js
@@ -5,40 +5,63 @@ import phrases from './puzzleBank/phrases.js';
 
 class App extends React.Component  {
   constructor(props) {
-  super(props);
-  // this.state = {currentPuzzle: }
+    super(props);
+    this.state = {
+      allLetters:
+        ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+      currentPuzzle: [],
+      currentTurnLetter: null,
+      lettersUsed: [],
+      allPuzzles: phrases,
+               wheelScoreValues: [100, 200, 300, 400, 500, 'bankrupt'],
+      runningScore : 0,
+      costOfVowel: 100,
+    };
   }
 
-  preparePuzzle() {
+    retrieveNewPuzzle = () => {
       let puzzle = phrases[Math.floor(Math.random() * phrases.length)];
-      puzzle = puzzle.split('');
-      return puzzle.map(function(letter, index){
-        if(letter !== ' '){
-          return (
-            <div key={index} className="puzzleWrapper">
-              <div className="puzzleLetter">{letter.toUpperCase()}</div>
-            </div>
-          )
-        }
-        else {
-          return(
-            <div key={index}>
-              &nbsp;
-            </div>
-          )
-        }
-      });
+      return puzzle.split('');
     }
 
+    componentWillMount = () => {
+      const puzzle = this.retrieveNewPuzzle();
+      this.setState({currentPuzzle : puzzle});
+    }
 
-  render() {
+    handleButtonRefresh = () => {
+      const puzzle = this.retrieveNewPuzzle();
+      this.setState({currentPuzzle : puzzle});
+    }
+
+  render = () => {
+    let puzzle = this.state.currentPuzzle;
+    //render puzzle spaces with ? showing for each space
+
+    //set score to 0
       return (
-      <div className="PuzzleBoard">
+      <div className="puzzleBoardWrapper">
         <div className="puzzleBoard">
-            {this.preparePuzzle() }
-            </div>
+            {puzzle.map(function(letter, index){
+              if(letter !== ' '){
+                return (
+                  <div key={index} className="puzzleWrapper">
+                    <div className="puzzleLetter">{letter.toUpperCase()}</div>
+                  </div>
+                )
+              }
+              else {
+                return(
+                  <div key={index}>
+                    &nbsp;
+                  </div>
+                )
+              }
+            }) }
+        </div>
+            <div className="runningScore"></div>
             <div>
-              <button>Get New Puzzle</button>
+              <button onClick={this.handleButtonRefresh}>Start New Game</button>
             </div>
       </div>
     );
