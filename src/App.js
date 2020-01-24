6a5ea22f6b060d2ea1e4076762f4ba648e6828ca
= <=>
2020-01-23 21:13:46 -0500

    add huge array of categories and puzzles, scoring now works except for bankrupt and lose a tjrn

--- src/App.js
+++ src/App.js
@@ -7,49 +7,58 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      allLetters: [
-        "a",
-        "b",
-        "c",
-        "d",
-        "e",
-        "f",
-        "g",
-        "h",
-        "i",
-        "j",
-        "k",
-        "l",
-        "m",
-        "n",
-        "o",
-        "p",
-        "q",
-        "r",
-        "s",
-        "t",
-        "u",
-        "v",
-        "w",
-        "x",
-        "y",
-        "z"
+      wheelScoreValues: [
+        400,
+        450,
+        500,
+        550,
+        600,
+        650,
+        700,
+        750,
+        800,
+        850,
+        900,
+        950,
+        1000,
+        2500,
+        3500,
+        5000,
+        "loseaturn",
+        "bankrupt"
       ],
-      currentPuzzle: [],
-      currentTurnLetter: "",
-      allUsedLetters: [],
+      vowels: ["a", "e", "i", "o", "u"],
+      specialChars: ["&", "'", "!", "?", ","],
       allPuzzles: phrases,
-      wheelScoreValues: [100, 200, 300, 400, 500, "bankrupt"],
+      currentPuzzle: [],
+      allUsedLetters: [],
+      currentTurnLetter: "",
       runningScore: 0,
       costOfVowel: 100,
-      specialChars: ["&", "'", "!", "?", ","],
-      correctlyGuessedLetters: []
+      correctlyGuessedLetters: [],
+      currentScoreValue: 0,
+      correctLetterCount: 0
     };
   }
 
   retrieveNewCategory = () => {
+    const categories = Object.keys(phrases);
     const category = categories[Math.floor(Math.random() * categories.length)];
-    return category;
+    this.setState({ currentCategory: category });
+    const puzzle =
+      phrases[category][Math.floor(Math.random() * phrases[category].length)];
+    return puzzle;
+  };
+
+  //Figure out why this is not working - Jan 21, 2019
+  getDollarAmountPerTurn = () => {
+    const score = this.state.wheelScoreValues[
+      Math.floor(Math.random() * this.state.wheelScoreValues.length)
+    ];
+    this.setState({ currentScoreValue: score });
+    if (score === "bankrupt") {
+      this.setState({ runningScore: 0 });
+    }
   };
 
   renderPuzzle = () => {
@@ -60,30 +69,42 @@ class App extends React.Component {
       ) {
         return <span key={index}>{letter}</span>;
       } else if (letter === " ") {
-        return <span key={index}>&nbsp;&nbsp;</span>;
+        return <span key={index}>&nbsp;&nbsp;&nbsp;</span>;
       } else {
-        return <span key={index}>_ </span>;
+        return <span key={index}>___ </span>;
       }
     });
   };
 
-  componentWillMount = () => {
-    const category = this.retrieveNewCategory();
-    const puzzle = phrases[category].toUpperCase().split("");
-    this.setState({ currentCategory: category });
+  UNSAFE_componentWillMount = () => {
+    const puzzle = this.retrieveNewCategory()
+      .toUpperCase()
+      .split("");
     this.setState({ currentPuzzle: puzzle });
+    this.getDollarAmountPerTurn();
   };
 
   handleButtonRefresh = () => {
-    const category = this.retrieveNewCategory();
-    const puzzle = phrases[category].toUpperCase().split("");
-    this.setState({ currentCategory: category });
+    const puzzle = this.retrieveNewCategory()
+      .toUpperCase()
+      .split("");
     this.setState({ currentPuzzle: puzzle });
     this.setState({ correctlyGuessedLetters: [] });
     this.setState({ allUsedLetters: [] });
+    this.setState({ runningScore: 0 });
+    this.setState({ correctLetterCount: 0 });
+    this.getDollarAmountPerTurn();
   };
 
   handleInputTurnLetter = e => {
+    let count = 0;
+    this.state.currentPuzzle.map(letter => {
+      if (e.target.value.toUpperCase() === letter) {
+        count++;
+      }
+    });
+
+    this.setState({ correctLetterCount: count });
     this.setState({ currentTurnLetter: e.target.value.toUpperCase() });
   };
 
@@ -98,20 +119,58 @@ class App extends React.Component {
       }
       return true;
     });
+    if (
+      this.state.allUsedLetters.indexOf(
+        this.state.currentTurnLetter.toUpperCase()
+      ) !== -1
+    ) {
+      this.setState({
+        correctlyGuessedLetters: this.state.correctlyGuessedLetters
+      });
+    }
   };
 
-  handleSetStateallUsedLetters = () => {
-    this.setState({
-      allUsedLetters: this.state.allUsedLetters.concat([
-        this.state.currentTurnLetter
-      ])
+  handleSetStateAllUsedLetters = () => {
+    if (
+      this.state.allUsedLetters.indexOf(
+        this.state.currentTurnLetter.toUpperCase()
+      ) !== -1
+    ) {
+      this.setState({ allUsedLetters: this.state.allUsedLetters });
+    } else {
+      this.setState({
+        allUsedLetters: this.state.allUsedLetters.concat([
+          this.state.currentTurnLetter
+        ])
+      });
+    }
+  };
+
+  wrapperGuessLetterOnClick = () => {
+    this.handleSetStateAllUsedLetters();
+    this.handleSetStatecorrectlyGuessedLetters();
+    this.getDollarAmountPerTurn();
+    this.updateRunningScore();
+  };
+
+  updateRunningScore = () => {
+    return this.state.currentPuzzle.map((letter, index) => {
+      if (
+        this.state.currentTurnLetter === letter &&
+        this.state.allUsedLetters.indexOf(letter) === -1
+      ) {
+        this.setState({
+          runningScore:
+            this.state.runningScore +
+            this.state.currentScoreValue * this.state.correctLetterCount
+        });
+      } else {
+        return this.state.runningScore;
+      }
     });
   };
 
-  wrapperOnClick = () => {
-    this.handleSetStateallUsedLetters();
-    this.handleSetStatecorrectlyGuessedLetters();
-  };
+  updateScoreForVowels = () => {};
 
   render = () => {
     return (
@@ -139,7 +198,7 @@ class App extends React.Component {
           onChange={this.handleInputTurnLetter}
           type="text"
         ></input>
-        <button onClick={this.wrapperOnClick}>
+        <button onClick={this.wrapperGuessLetterOnClick}>
           I'll take the {this.state.currentTurnLetter}
         </button>
       </div>
---
+++ src/puzzleBank/samplePhrases.js
@@ -0,0 +1,14 @@
+export default {
+  Action: "DISAPPEARING ACT",
+  "Before & After": "BELLY & BREAK DANCING",
+  "Food & Drink": "Bagel with cream cheese",
+  Games: "wheel of fortune",
+  "Living Things": "GANDERS AND GEESE",
+  "Movie Title": "ON THE CUTTING EDGE",
+  People: "TEACHERS AND PUPILS",
+  Phrase: "It's a beautiful day in the neighborhood",
+  Thing: "OFFBEAT MOVIE",
+  "Toss up 1": "ROOSTERS AND HENS",
+  "Toss up 2": "STALLIONS AND MARES",
+  Travel: "TRAVELING THROUGH FRANCE"
+};
